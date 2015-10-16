e067bd7c63add0415aa379faaf1b583b5e696c4b
Toby Clarke <teeohbee@gmail.com>
2015-10-16 16:52:00 +0100

    Added ui router

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
-app.controller('MainCtrl', ['$scope', function($scope) {
+  app.factory('posts', [function() {
+    var object = {
+      posts: []
+    };
+    return object;
+  }])
+
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
   $scope.test = 'Hello world!';
 
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+  $scope.posts = posts.posts;
 
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0
+    });
     $scope.title = '';
+    $scope.link = '';
   };
 
   $scope.incrementUpvotes = function(post) {
     post.upvotes += 1
   }
+
 
 }]);

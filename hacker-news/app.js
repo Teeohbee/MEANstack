7108727b7cf15cf6593debb9b8c3ae6c58e8bd8e
Toby Clarke <teeohbee@gmail.com>
2015-10-19 18:40:47 +0100

    Added upvote routing

--- hacker-news/app.js
+++ hacker-news/app.js
@@ -1,20 +1,18 @@
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+
+mongoose.connect('mongodb://localhost/news');
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-require('./models/Posts');
-require('./models/Comments');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/news');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
